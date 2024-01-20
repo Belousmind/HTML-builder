02c6ef7547a2e75989a3a1cc0bcc9cb888505e56
Darya Belousova <belousmind@gmail.com>
2024-01-20 19:11:08 +0100

    fix: replace 'console.log' on 'stdout'

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const { stdin, stdout } = require('node:process');
 
 const read = fs.createReadStream(
   '01-read-file/text.txt', {encoding: 'utf8'});
 
-read.on('data', (chunk) => console.log(chunk))
+read.on('data', (chunk) => process.stdout.write(chunk))
+
+
