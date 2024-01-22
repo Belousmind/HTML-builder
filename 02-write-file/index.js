285b69323b992c900cf098bca84cd44517ec0028
Darya Belousova <belousmind@gmail.com>
2024-01-22 15:55:53 +0100

    feat: continued to solve the second task & try method mkdir in the fourth task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
+const { stdin, stdout } = require('node:process');
 const newFile = fs.createWriteStream('02-write-file/new-file.txt');
 
-// newFile.write();
+stdout.write('Hey! Write something here: ')
+
+stdin.on('data', (chunk) => {
+  newFile.write(chunk);
+  if (chunk === 'exit') {
+    stdout.write('Stream is end');
+    exit()
+  }
+})
+
+
---
+++ 04-copy-directory/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('node:path'); 
+ 
+fs.mkdir(path.join(__dirname, 'files-copy'),
+    (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory created successfully!');
+    });
