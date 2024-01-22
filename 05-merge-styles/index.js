5dbd01597fea5564fb2e0740e672f5d403702579
Darya Belousova <belousmind@gmail.com>
2024-01-22 20:25:14 +0100

    feat: solve the fifth taks

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 const path = require('node:path');
 
-const projectPath = path.join(__dirname, 'project-dist');
+const bundleFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
 
 function bundleCreator() {
-
+  fs.readdir(path.resolve(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => { 
+      if (err) 
+        console.log(err); 
+      else {
+        files.forEach(file => {
+          if (file.isFile() && path.extname(`${file.name}`) === '.css') {
+            const styleData = fs.createReadStream(path.join(__dirname, 'styles', `${file.name}`), {encoding: 'utf8'});
+            styleData.on('data', (chunk) => bundleFile.write(chunk));
+          }
+        })
+      } 
+    }
+  )
 }
+
+bundleCreator();
+
+
