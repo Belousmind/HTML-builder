6398fcf9c33105cf7d89eff93aae0dea0fa510a9
Darya Belousova <belousmind@gmail.com>
2024-01-22 19:32:31 +0100

    feat: completed the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,33 @@
 const fs = require('fs');
 const path = require('node:path'); 
- 
-fs.mkdir(path.join(__dirname, 'files-copy'),
-    (err) => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('Directory created successfully!');
-    });
+
+const dir = path.join(__dirname, 'files');
+
+function copyAlert(err) {
+    if (err) throw err;
+    console.log('file was copped');
+  }
+
+function makeCopy(dir) {
+    fs.readdir(dir,
+        { withFileTypes: true },
+        (err, files) => { 
+            if (err) 
+              console.log(err); 
+            else {
+                fs.mkdir(path.join(__dirname, 'files-copy'),
+                { recursive: true },
+                (err) => {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    files.forEach(file => {
+                        fs.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`), copyAlert); 
+                    }); 
+                });  
+            } 
+          }
+      ) 
+}
+
+makeCopy(dir);
