3af3a68220122dbc71c6307646e9db5d62484334
belousmind <belousmind@gmail.com>
2025-01-19 12:36:12 +0300

    fix: html file assembly, and also improved the rest of the code along the way

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,116 +1,119 @@
 const fs = require('fs');
 const path = require('node:path');
 
-const css = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
-
 function pageBuilder() {
-  fs.mkdir(path.join(__dirname, 'project-dist'),
-  { recursive: true },
-  (err) => {
+  fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
     if (err) {
-        return console.error(err);
+      console.error(err);
+      return;
     }
     htmlTemplateCopy();
     stylesBundler();
-    makeCopy();
-  });  
+    copyAssets();
+  });
 }
 
 function stylesBundler() {
-  fs.readdir(path.resolve(__dirname, 'styles'),
-    { withFileTypes: true },
-    (err, files) => { 
-        if (err) 
-          console.log(err); 
-        else {
-          files.forEach(file => {
-            if (file.isFile() && path.extname(`${file.name}`) === '.css') {
-              const styleData = fs.createReadStream(path.join(__dirname, 'styles', `${file.name}`), {encoding: 'utf8'});
-              styleData.on('data', (chunk) => css.write(chunk));
-            }
-          })
-        } 
-      }
-    )
-}
+  const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
 
-function copyAlert(err) {
-  if (err) throw err;
-  // console.log('file was copped');
-}
-
-function makeCopy() {
-  fs.readdir(path.resolve(__dirname, 'assets'),
-  { withFileTypes: true,
-    recursive: true },
-  (err, dirs) => { 
-      if (err) 
-        console.log(err); 
-      else {
-        dirs.forEach(dir => {
-          fs.readdir(path.resolve(__dirname, 'assets', `${dir.name}`),
-          { withFileTypes: true,
-            recursive: true },
-            (err, files) => { 
-              if (err) 
-                console.log(err); 
-              else {
-                fs.mkdir(path.join(__dirname, 'project-dist', 'assets', `${dir.name}`),
-                { recursive: true },
-                (err) => {
-                  if (err) {
-                      return console.error(err);
-                  }
-                  files.forEach(file => {
-                    fs.copyFile(path.join(__dirname, 'assets', `${dir.name}`, `${file.name}`), path.join(__dirname, 'project-dist', 'assets', `${dir.name}`, `${file.name}`), copyAlert); 
-                    }); 
-                });  
-              } 
-            }
-          )
-        })
-      } 
+  fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  )
+    files.forEach(file => {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const input = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf8');
+        input.pipe(output);
+      }
+    });
+  });
+}
+
+function copyAssets() {
+  const src = path.join(__dirname, 'assets');
+  const dest = path.join(__dirname, 'project-dist', 'assets');
+
+  function copyDirectory(srcDir, destDir) {
+    fs.mkdir(destDir, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      fs.readdir(srcDir, { withFileTypes: true }, (err, entries) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        entries.forEach(entry => {
+          const srcPath = path.join(srcDir, entry.name);
+          const destPath = path.join(destDir, entry.name);
+
+          if (entry.isDirectory()) {
+            copyDirectory(srcPath, destPath);
+          } else {
+            fs.copyFile(srcPath, destPath, (err) => {
+              if (err) console.error(err);
+            });
+          }
+        });
+      });
+    });
+  }
+
+  copyDirectory(src, dest);
 }
 
 function htmlTemplateCopy() {
-  fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist', 'index.html'), copyAlert);
-  htmlBundler()
-}
+  const templatePath = path.join(__dirname, 'template.html');
+  const outputPath = path.join(__dirname, 'project-dist', 'index.html');
 
-function htmlBundler() {
-  fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 
-  {encoding: 'utf8'},
-  (err, content) => {
-    if (err) 
-        console.log(err);
-    else {
-      let html = content;
-      // console.log(html)
-      fs.readdir(path.join(__dirname, 'components'),
-      { withFileTypes: true },
-      (err, files) => { 
-          if (err) 
-            console.log(err); 
-          else {
-            files.forEach(file => {
-              const moduleName = `{{${path.basename(`${file.name}`, '.html')}}}`;
-              // console.log(moduleName);
-              if (html.includes(moduleName)) {
-                fs.readFile(path.join(__dirname, 'components', `${file.name}`), 
-                {encoding: 'utf8'},
-                (err, content) => {
-                  html = html.replace(moduleName, content);
-                  fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'), {encoding: 'utf8'}).write(html);
-                })
-              }
-            })
-          } 
-        }
-      )
+  fs.readFile(templatePath, 'utf8', (err, template) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  })
+
+    fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      let pending = files.length;
+
+      files.forEach(file => {
+        if (file.isFile() && path.extname(file.name) === '.html') {
+          const componentName = `{{${path.basename(file.name, '.html')}}}`;
+          fs.readFile(path.join(__dirname, 'components', file.name), 'utf8', (err, componentContent) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            template = template.replace(new RegExp(componentName, 'g'), componentContent);
+            pending--;
+
+            if (pending === 0) {
+              fs.writeFile(outputPath, template, 'utf8', (err) => {
+                if (err) console.error(err);
+              });
+            }
+          });
+        } else {
+          pending--;
+        }
+      });
+
+      if (pending === 0) {
+        fs.writeFile(outputPath, template, 'utf8', (err) => {
+          if (err) console.error(err);
+        });
+      }
+    });
+  });
 }
 
 pageBuilder();
